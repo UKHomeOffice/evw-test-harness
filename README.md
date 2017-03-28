1fd6e7ed6acfd1459d721a1a72cd9a417167bfc2
Ralph Cowling <speak@ralph-cowling.com>
2017-03-28 10:27:40 +0100

    ⚰ hardcoded test files ⚰

---
+++ README.md
@@ -0,0 +1,11 @@
+### evw test harness 🔍
+
+*to use:*
+
+```
+git clone
+npm i
+# take a failing case from customer db
+# save it to evws/test.js
+node test
+```
---
+++ test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const chalk = require('chalk');
+const Validator = require('jsonschema').Validator;
+const transform = require('evw-schemas').evw.hof.transform;
+const schema = require('evw-schemas').evw.mainForm.schema;
+const data = require('./evws/test');
+const v = new Validator();
+const log = console.log;
+
+const validate = (data, schema) => {
+  const result = v.validate(data, schema);
+  if (result.valid) {
+    return Promise.resolve(result);
+  }
+  return Promise.reject(result);
+};
+
+const testApplication = () => {
+  transform
+  .transformData(data)
+  .then(transformedData => {
+    log(chalk.cyan('Testing application...'));
+    log(chalk.cyan('transformed data:'));
+    console.dir(transformedData, {colors: true});
+    log(chalk.cyan('-----'));
+    validate(transformedData, schema)
+    .then(data => log(`
+      ${chalk.green('record is valid')}
+      ${data}
+    `))
+    .catch(result => {
+      log(chalk.yellow(`record has failed validation
+
+validation errors:`));
+      result.errors.forEach((error, i) => {
+        log(`
+⁍ ${i+1}: ${chalk.red(error)}
+        `);
+        console.dir(error, {
+          colors: true,
+          depth: 4
+        });
+      });
+    });
+  })
+  .catch(e => {
+    console.error('error', e);
+  });
+};
+
+testApplication();
